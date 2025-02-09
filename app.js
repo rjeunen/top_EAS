db702c0690d584940e651da7c09c40903b9e4b39
Robby Jeunen <robbyjeunen@gmail.com>
2025-02-09 13:45:59 +0100

    added dynamic functionality to create a div

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const gridContainer = document.querySelector("#gridContainer");
 const gridArea = 600;
 const rows = 16;
 const cols = 16;
+const widthDiv = gridArea / rows;
+const heightDiv = gridArea / cols;
 
 gridContainer.style.width = `${gridArea}px`;
 gridContainer.style.height = `${gridArea}px`;
@@ -11,7 +13,9 @@ gridContainer.style.backgroundColor = `coral`;
 function createGrid(){
     for(i = 0; i < (rows * cols); i++){
         const div = document.createElement("div");
-        //div.style.border = "1px solid black";
+        div.style.width = `${widthDiv}px`;
+        div.style.height = `${heightDiv}px`;
+        div.style.border = "1px solid black";
         gridContainer.appendChild(div);
     }
 }
